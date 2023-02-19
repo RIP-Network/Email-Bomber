91eda6977aa6933014f22a94c4c35c2fe4ee088a
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2023-02-20 00:16:02 +0100

    Update email.py

--- email.py
+++ email.py
@@ -22,23 +22,14 @@ time.sleep(2.0)
 print(amarillo+"Vamos al Script"+cierre)
 time.sleep(1.5)
 os.system("clear")
-print(rojo+" ____________ "+cierre)
-print(blanco+" < RIPspam >"+cierre)
-print(rojo+" ------------"+cierre)
-print(rojo+"▒▒▒▒▒▒▒▒▄▄▄▄▄▄▄▄▒▒▒▒▒▒ "+cierre)
-print(rojo+"▒▒█▒▒▒▄██████████▄▒▒▒▒ "+cierre)
-print(rojo+"▒█▐▒▒▒████████████▒▒▒▒ "+cierre + azul+"[*]Sistemas"+cierre)
-print(rojo+"▒▌▐▒▒██▄▀██████▀▄██▒▒▒ "+cierre)
-print(rojo+"▐┼▐▒▒██▄▄▄▄██▄▄▄▄██▒▒▒ "+cierre + blanco+" Linux Y Termux"+cierre)
-print(rojo+"▐┼▐▒▒██████████████▒▒▒ "+cierre)
-print(rojo+"▐▄▐████─▀▐▐▀█─█─▌▐██▄▒ "+cierre)
-print (rojo+"▒▒█████──────────▐███▌"+cierre)
-print(rojo+"▒▒█▀▀██▄█─▄───▐─▄███▀▒ "+cierre + amarillo+"Derechos de autor")
-print(rojo+"▒▒█▒▒███████▄██████▒▒▒ "+cierre + blanco + "  RIP NETWORK "+ cierre)
-print(rojo+"▒▒▒▒▒██████████████▒▒▒ "+cierre)
-print(rojo+"▒▒▒▒▒█████████▐▌██▌▒▒▒ "+cierre)
-print(rojo+"▒▒▒▒▒▐▀▐▒▌▀█▀▒▐▒█▒▒▒▒▒ "+cierre + blanco + "Legion Guardians"+ cierre)
-print(rojo+"▒▒▒▒▒▒▒▒▒▒▒▐▒▒▒▒▌▒▒▒▒▒ "+cierre)
+
+try:
+	file1 = open('Banner.txt', 'r')
+	print(' ')
+	file1.close()
+except IOError:
+	print('Banner Not found')
+
 time.sleep(1.0)
 print("\n")                                                                             
 c=input(amarillo+"Ingresar su cuenta de Gmail :" + cierre)
@@ -52,9 +43,8 @@ mensaje=input(azul+"Escriba el mensaje:" + cierre)
 while True:
     context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL("smtp-mail.outlook.com", 587, context=context) as server:
-        server.login(c, p)
-        server.sendmail(c, destinatario, mensaje)
-        print(rojo+"Correo enviado"+cierre)
-
+    server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+    server.login(c, p)
+    server.sendmail(c, destinatario, mensaje)
+    print(rojo+"Correo enviado"+cierre)
 
